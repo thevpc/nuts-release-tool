ca79be8037f8e80cd187fa772a43f35344a1a08b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update BaseConfRunner.java : refactor NMsg

--- nuts-release-tool/src/main/java/net/thevpc/nuts/build/builders/BaseConfRunner.java
+++ nuts-release-tool/src/main/java/net/thevpc/nuts/build/builders/BaseConfRunner.java
@@ -10,7 +10,7 @@ import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NAssert;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 /**
  * @author vpc
