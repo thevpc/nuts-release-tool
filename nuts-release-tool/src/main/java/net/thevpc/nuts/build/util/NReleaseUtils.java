df3d32f18337c4813e39c132b11a5acfb1fbfd19
thevpc <taha.bensalah@gmail.com>
2025-11-17 00:43:18 +0100

    Update NReleaseUtils.java : added-japi-compat

--- nuts-release-tool/src/main/java/net/thevpc/nuts/build/util/NReleaseUtils.java
+++ nuts-release-tool/src/main/java/net/thevpc/nuts/build/util/NReleaseUtils.java
@@ -3,16 +3,47 @@ package net.thevpc.nuts.build.util;
 import net.thevpc.nuts.artifact.NDescriptor;
 import net.thevpc.nuts.artifact.NDescriptorParser;
 import net.thevpc.nuts.artifact.NDescriptorStyle;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementType;
+import net.thevpc.nuts.elem.NOperatorElement;
+import net.thevpc.nuts.elem.NPairElement;
 import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class NReleaseUtils {
     public static void ensureNutsRepoFolder(NPath currentDir) {
         if (!isNutsRepoFolder(currentDir)) {
             throw new NIllegalArgumentException(NMsg.ofC("you must run release tool under nuts github repository root folder. Now we are under %s", currentDir));
         }
+    }
+
+    public static Map<String, NElement> asNamedPairs(NElement elems) {
+        LinkedHashMap<String, NElement> pars = new LinkedHashMap<>();
+        if (elems != null && elems.asObject().isPresent()) {
+            for (NElement c : elems.asObject().get().children()) {
+                if (c.isNamedPair()) {
+                    NPairElement p = c.asPair().get();
+                    String key = p.key().asStringValue().get();
+                    pars.put(key, p.value());
+                } else if (
+                        c.isBinaryOperator(NElementType.OP_EQ)
+                                || c.isBinaryOperator(NElementType.OP_COLON_EQ)
+                ) {
+                    NOperatorElement o = c.asOperator().get();
+                    NElement f = o.first().get();
+                    NElement s = o.second().get();
+                    if (f.isAnyString()) {
+                        pars.put(f.asStringValue().get(), s);
+                    }
+                }
+            }
+        }
+        return  pars;
     }
 
     public static boolean isNutsRepoFolder(NPath currentDir) {
